f91076f4d4084b137043d6135116f39ffc31f62d
l30simoes <l30simoes@gmail.com>
2018-03-14 10:16:18 +0100

    Add files via upload

--- 1U/ultimate_1U.py
+++ 1U/ultimate_1U.py
@@ -18,7 +18,17 @@ sys.path.append(os.path.join(sys.path[0], '../'))
 from instabot import Bot
 
 #bot = Bot()
-bot = Bot(max_follows_per_day=1000)
+bot = Bot(
+            # Max
+            max_follows_per_day=1000,
+            max_likes_per_day=1000,
+            max_unfollows_per_day=500,
+
+            # Delay
+            like_delay=10,
+            unfollow_delay=10,
+            follow_delay=20
+          )
 # bot.login()
 bot.login(username="1sturban", password="FirstU12#")
 
@@ -28,28 +38,38 @@ bot.login(username="1sturban", password="FirstU12#")
 filePath = os.path.dirname(os.path.abspath(__file__)) + "/"
 
 
-follow_followers_list = bot.read_list_from_file(filePath + "follow_followers_1U.txt")
+print("Current script's schedule:")
+
+follow_followers_list = bot.read_list_from_file("follow_followers_1U.txt")
 print("Going to follow followers of:", follow_followers_list)
 
-follow_following_list = bot.read_list_from_file(filePath + "follow_following_1U.txt")
+follow_following_list = bot.read_list_from_file("follow_following_1U.txt")
 print("Going to follow following of:", follow_following_list)
 
-like_hashtags_list = bot.read_list_from_file(filePath + "like_hashtags_1U.txt")
+like_hashtags_list = bot.read_list_from_file("like_hashtags_1U.txt")
 print("Going to like hashtags:", like_hashtags_list)
 
-# like_users_list = bot.read_list_from_file(filePath + "like_users_1U.txt")
-# print("Going to like users:", like_users_list)
+like_users_list = bot.read_list_from_file("like_users_1U.txt")
+print("Going to like users:", like_users_list)
+
 
 tasks_list = []
+
+# tasks_list.append((bot.unfollow_non_followers()))
+
 for item in follow_followers_list:
     tasks_list.append((bot.follow_followers, {'user_id': item, 'nfollows': None}))
+
 for item in follow_following_list:
     tasks_list.append((bot.follow_following, {'user_id': item}))
+
 for item in like_hashtags_list:
     tasks_list.append((bot.like_hashtag, {'hashtag': item, 'amount': None}))
 # for item in like_users_list:
-#     tasks_list.append((bot.like_user, {'user_id': item, 'amount': None}))
 
-shuffle(tasks_list)
+
+
+# shuffle(tasks_list)
+
 for func, arg in tasks_list:
     func(**arg)
