ace5af1d7ce29b0ba60fdfe4efcacff54fb93dce
l30simoes <l30simoes@gmail.com>
2018-03-14 13:10:48 +0100

    Add files via upload

--- 1U/ultimate_1U.py
+++ 1U/ultimate_1U.py
@@ -18,24 +18,34 @@ sys.path.append(os.path.join(sys.path[0], '../'))
 from instabot import Bot
 
 #bot = Bot()
+
+#TXT File Path ///
+filePath = os.path.dirname(os.path.abspath(__file__)) + "/"
+
+comments_file_name = filePath + "comments.txt"
+
 bot = Bot(
             # Max
             max_follows_per_day=1000,
             max_likes_per_day=1000,
             max_unfollows_per_day=500,
+            max_comments_per_day=1200,
 
             # Delay
             like_delay=10,
             unfollow_delay=10,
-            follow_delay=20
+            follow_delay=20,
+            comment_delay=25,
+
+            comments_file=comments_file_name
+
           )
 # bot.login()
 bot.login(username="1sturban", password="FirstU12#")
 
+# bot.login(username="1nsta_bot001", password="Insta12#")
 
-
-#TXT File Path ///
-filePath = os.path.dirname(os.path.abspath(__file__)) + "/"
+print ("Comment file: " + comments_file_name)
 
 
 print("Current script's schedule:")
@@ -49,26 +59,30 @@ print("Going to follow following of:", follow_following_list)
 like_hashtags_list = bot.read_list_from_file(filePath + "like_hashtags_1U.txt")
 print("Going to like hashtags:", like_hashtags_list)
 
-like_users_list = bot.read_list_from_file(filePath + "like_users_1U.txt")
-print("Going to like users:", like_users_list)
+# like_users_list = bot.read_list_from_file(filePath + "like_users_1U.txt")
+# print("Going to like users:", like_users_list)
 
 
 tasks_list = []
 
 
 
+
 for item in follow_followers_list:
     tasks_list.append((bot.follow_followers, {'user_id': item, 'nfollows': None}))
 
-for item in follow_following_list:
-    tasks_list.append((bot.follow_following, {'user_id': item}))
+# for item in follow_following_list:
+#     tasks_list.append((bot.follow_following, {'user_id': item}))
 
 for item in like_hashtags_list:
     tasks_list.append((bot.like_hashtag, {'hashtag': item, 'amount': None}))
-# for item in like_users_list:
 
-# tasks_list.append((bot.unfollow_non_followers()))
+for item in like_hashtags_list:
+    tasks_list.append((bot.comment_hashtag, {'hashtag': item, 'amount': None}))
 
+
+
+# print ("TASK LIST - " + tasks_list)
 
 # shuffle(tasks_list)
 
