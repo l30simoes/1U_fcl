5e91d2fa294838995d017dbebdb1efa42eefebfa
l30simoes <l30simoes@gmail.com>
2018-03-18 09:01:49 +0100

    Add files via upload

---
+++ 1U/1U_unfollow_non_followers.py
@@ -0,0 +1,26 @@
+"""
+    instabot example
+
+    Workflow:
+        1) unfollows users that don't follow you.
+"""
+
+import sys
+import os
+import argparse
+
+sys.path.append(os.path.join(sys.path[0], '../../'))
+
+from instabot import Bot
+
+parser = argparse.ArgumentParser(add_help=True)
+parser.add_argument('-u', type=str, help="username")
+parser.add_argument('-p', type=str, help="password")
+parser.add_argument('-proxy', type=str, help="proxy")
+args = parser.parse_args()
+
+bot = Bot(max_unfollows_per_day=500)
+
+bot.login(username='1sturban', password='FirstU12#',
+          proxy=args.proxy)
+bot.unfollow_non_followers()
